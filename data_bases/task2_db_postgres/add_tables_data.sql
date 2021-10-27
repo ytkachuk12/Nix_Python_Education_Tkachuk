4d2bcc891e8baab53f02306aeaec25d226856cf1
Yurii Tkachuk <ytkachuk12@gmail.com>
2021-10-27 23:13:51 +0300

    fix add_tables_data file

---
+++ data_bases/task2_db_postgres/add_tables_data.sql
@@ -0,0 +1,13 @@
+COPY Users FROM '/usr/src/users.csv' DELIMITER ',' CSV;
+
+COPY Carts FROM '/usr/src/carts.csv' DELIMITER ',' CSV;
+
+COPY Order_status FROM '/usr/src/order_statuses.csv' DELIMITER ',' CSV;
+
+COPY Orders FROM '/usr/src/orders.csv' DELIMITER ',' CSV;
+
+COPY Categories FROM '/usr/src/categories.csv' DELIMITER ',' CSV;
+
+COPY Products FROM '/usr/src/products.csv' DELIMITER ',' CSV;
+
+COPY Cart_product FROM '/usr/src/cart_products.csv' DELIMITER ',' CSV;
